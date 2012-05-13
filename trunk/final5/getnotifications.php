93001bb6534e2c491b975eabd89ede9f3ef70bac
kyk25@cornell.edu <kyk25@cornell.edu@44689d61-b3be-0178-2498-c092c6eb7645>
2012-05-13 12:16:20 +0000

    Finished blogs. Almost done with events. Trying to get RSVP to work and then will be done.

---
+++ trunk/final5/getnotifications.php
@@ -0,0 +1,14 @@
+<?
+session_start();
+include_once('db.inc');
+$mysql = new mysqli($host, $username, $password, $database);
+$result = $mysql->query("SELECT type FROM notifications WHERE datetime > (SELECT lastViewed FROM notificationsViewed WHERE username ='".$_SESSION['logged_user']."') LIMIT 1");
+$notification = $result->fetch_assoc();
+$type = $notification['type'];
+$response = "";
+if($type == 'photo') {
+	$response = "A new photo has been added.";
+	$mysql->query("UPDATE notificationsViewed SET lastViewed = NOW() WHERE username ='".$_SESSION['logged_user']."'");
+}
+echo $response;
+?>
